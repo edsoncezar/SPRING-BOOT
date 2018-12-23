bff80a7bfc6fff4dfa0c0275e88ac4d4795fbe6d
Edson Cezar <edsonmcz@gmail.com>
2018-12-23 19:55:30 -0200

    Refactoring to improve understanding

--- src/test/java/com/edson/server/main/SpringBootRest2ApplicationTests.java
+++ src/test/java/com/edson/server/main/SpringBootRest2ApplicationTests.java
@@ -1,6 +1,5 @@
 package com.edson.server.main;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,36 +18,36 @@ public class SpringBootRest2ApplicationTests {
 
 	@Test
 	public void getListOfHotel() {
-		
+
 		HotelRetrieveController control = new HotelRetrieveController();
-		
+
 		ResponseEntity<City> city = control.getListOfHotel(1);
-		
+
 		City objects = city.getBody();
 		MediaType contentType = city.getHeaders().getContentType();
 		HttpStatus statusCode = city.getStatusCode();
-		
-        System.out.println(objects);
-        System.out.println(contentType);
-        System.out.println(statusCode);
-		 
+
+		System.out.println(objects);
+		System.out.println(contentType);
+		System.out.println(statusCode);
+
 	}
-	
+
 	@Test
 	public void getListOfCity() {
-		
+
 		HotelByCityRetrieveController control = new HotelByCityRetrieveController();
-		
+
 		ResponseEntity<City> city = control.getListOfCity(1032);
-		
+
 		City objects = city.getBody();
 		MediaType contentType = city.getHeaders().getContentType();
 		HttpStatus statusCode = city.getStatusCode();
-		
-        System.out.println(objects);
-        System.out.println(contentType);
-        System.out.println(statusCode);
-		 
+
+		System.out.println(objects);
+		System.out.println(contentType);
+		System.out.println(statusCode);
+
 	}
 
 }
